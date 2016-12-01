a667e2bb197361a62244b18f474c36d1fe0d2556
MasterGowen <mastergowen@gmail.com>
2016-12-01 21:10:03 +0500

    reversion + CourseAdmin

--- open_programs/settings.py
+++ open_programs/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'reversion',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
