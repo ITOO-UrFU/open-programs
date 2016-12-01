a6f131d79241b09bfa15c6fce67f79e78967b003
MasterGowen <mastergowen@gmail.com>
2016-12-01 18:58:19 +0500

    base settings #2

--- open_programs/urls.py
+++ open_programs/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 ]
+
+
+if settings.DEBUG is True:
+    urlpatterns.append(static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
+    urlpatterns.append(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
