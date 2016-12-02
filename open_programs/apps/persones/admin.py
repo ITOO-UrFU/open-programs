c8ee5db9950ff4e3c949ee07024d0606b9d4c06a
vesloguzov <vesloguzov@gmail.com>
2016-12-02 14:30:21 +0500

    Person in admin

--- open_programs/apps/persones/admin.py
+++ open_programs/apps/persones/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from reversion.admin import VersionAdmin
 
-# Register your models here.
+from .models import Person
+
+
+@admin.register(Person)
+class CourseAdmin(VersionAdmin):
+    fields = ("first_name", "second_name", "last_name", "sex", "alt_email", "country", "birthday_date", "biography")
+    list_display = ("first_name", "second_name", "last_name", "sex", "alt_email", "country", "birthday_date", "biography")
--- open_programs/apps/persones/models.py
+++ open_programs/apps/persones/models.py
@@ -3,9 +3,9 @@ from django.utils.translation import ugettext_lazy as _
 from django_countries.fields import CountryField
 
 class Person(models.Model):
-    first_name = models.CharField(_('Имя пользователя'), max_length=32, blank=False, default=_('Имя'))
-    last_name = models.CharField(_('Фамилия пользователя'), max_length=32, blank=False, default=_('Фамилия'))
-    second_name = models.CharField(_('Отчество пользователя'), max_length=32, blank=True, default=_('Отчество'))
+    first_name = models.CharField(_('Имя пользователя'), max_length=32, blank=False, default='')
+    last_name = models.CharField(_('Фамилия пользователя'), max_length=32, blank=False, default='')
+    second_name = models.CharField(_('Отчество пользователя'), max_length=32, blank=True, default='')
     SEXES = (
         ('U', _('Не выбран')),
         ('F', _('Женский')),
@@ -19,7 +19,7 @@ class Person(models.Model):
     country = CountryField()
     birthday_date = models.DateField(_('Дата рождения'), blank=True)
 #   roles = models.ManyToManyField(ROLE)  TODO: Сделать модель 'Роль'
-    biography = models.TextField(_('Биография пользователя'), blank=True, default=_('Биография'))
+    biography = models.TextField(_('Биография пользователя'), blank=True, default='')
 
 
 class AcademicDegree(models.Model):
