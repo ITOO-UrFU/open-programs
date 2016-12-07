16b14c7be5d45759c0cbbf5987df3a531b6d2d7d
MasterGowen <mastergowen@gmail.com>
2016-12-07 18:35:16 +0500

    add Profession model

--- open_programs/apps/professions/models.py
+++ open_programs/apps/professions/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from base.models import ObjectBaseClass
 
-# Create your models here.
+from permission import add_permission_logic
+from permission.logics import StaffPermissionLogic
+
+from persons.models import Person
+
+from django.utils.translation import ugettext_lazy as _
+
+
+class Profession(ObjectBaseClass):
+    title = models.CharField(_("Название профессии"), max_length=256, blank=False, default=_(""))
+    description = models.TextField(_("Описание профессии"), max_length=16384, blank=True, default=_("Здесь должно быть описание профессии"))
+
+    class Meta:
+        verbose_name = 'профессия'
+        verbose_name_plural = 'профессии'
+
+    def __str__(self):
+        return str(self.title)
+
+    @models.permalink
+    @classmethod
+    def get_absolute_url(self):
+        return None
+
+add_permission_logic(Profession, StaffPermissionLogic())
