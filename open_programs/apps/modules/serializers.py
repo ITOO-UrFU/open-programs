a97c5ea37764fb06324d9d00b9caa6bb7c10fcae
MasterGowen <mastergowen@gmail.com>
2016-12-17 19:33:29 +0500

    fix module api

--- open_programs/apps/modules/serializers.py
+++ open_programs/apps/modules/serializers.py
@@ -1,27 +1,36 @@
 from .models import Type, Module, GeneralBaseModulesPool, EducationalProgramTrajectoriesPool, ChoiceModulesPool
 from disciplines.models import Discipline
+from disciplines.serializers import DisciplineSerializer
 from rest_framework import serializers
-
-
-class ModuleSerializer(serializers.HyperlinkedModelSerializer):
-
-    disciplines = serializers.HyperlinkedRelatedField(
-        many=True,
-        read_only=False,
-        view_name='discipline-detail',
-        queryset=Discipline.objects.all(),
-        lookup_field="name"
-    )
-
-    class Meta:
-        model = Module
-        fields = ("id", "title", "description", "disciplines", "type", "results", "results_text", "competences", "status", "archived", "created", "updated")
 
 
 class TypeSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Type
         fields = ("title", "description", "status", "archived", "created", "updated")
+
+
+class ModuleSerializer(serializers.HyperlinkedModelSerializer):
+
+    type = TypeSerializer(
+        many=False,
+        read_only=False,
+        # view_name='discipline-detail',
+        # queryset=Discipline.objects.all(),
+        # lookup_field="name"
+    )
+
+    disciplines = DisciplineSerializer(
+        many=True,
+        read_only=True,
+        # view_name='discipline-detail',
+        # queryset=Discipline.objects.all(),
+        # lookup_field="name"
+    )
+
+    class Meta:
+        model = Module
+        fields = ("id", "title", "description", "disciplines", "type", "results", "results_text", "competences", "status", "archived", "created", "updated")
 
 
 class GeneralBaseModulesPoolSerializer(serializers.HyperlinkedModelSerializer):
