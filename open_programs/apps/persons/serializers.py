5552d633d8a7c6b9e797bdff2408f2930773c91f
MasterGowen <mastergowen@gmail.com>
2016-12-18 00:13:19 +0500

    clear migrations

--- open_programs/apps/persons/serializers.py
+++ open_programs/apps/persons/serializers.py
@@ -3,13 +3,18 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 
 
-class PersonSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Person
-        fields = ("user", "first_name", "last_name", "second_name", "sex", "alt_email", "birthday_date", "biography")  # "country" is not JSON serializable
-
-
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ("id", "username", "email")
+
+
+class PersonSerializer(serializers.HyperlinkedModelSerializer):
+    user = UserSerializer(
+        many=False,
+        read_only=False,
+    )
+    
+    class Meta:
+        model = Person
+        fields = ("user", "first_name", "last_name", "second_name", "sex", "alt_email", "birthday_date", "biography")  # "country" is not JSON serializable
