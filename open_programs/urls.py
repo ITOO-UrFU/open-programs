86d8bd53d0988e10af56cbbcd601cb1933099742
MasterGowen <mastergowen@gmail.com>
2016-12-20 12:41:09 +0500

    remove nested from urls

--- open_programs/urls.py
+++ open_programs/urls.py
@@ -56,7 +56,6 @@ schema_view = get_swagger_view(title='Open programs')
 urlpatterns = [
     url(r'^admin/', include('smuggler.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^nested_admin/', include('nested_admin.urls')),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^api/v1/', include(router.urls, namespace='api')),
     url(r'^api/docs/', schema_view),
