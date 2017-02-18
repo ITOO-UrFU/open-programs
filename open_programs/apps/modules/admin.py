107c06fbec607d67c4a571aa543f97bbae6d782a
MasterGowen <mastergowen@gmail.com>
2017-02-18 18:18:25 +0500

    some models

--- open_programs/apps/modules/admin.py
+++ open_programs/apps/modules/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from reversion.admin import VersionAdmin
 
-from .models import Module, GeneralBaseModulesPool, EducationalProgramTrajectoriesPool, ChoiceModulesPool, Type
+from .models import Module, Type
 
 
 @admin.register(Module)
@@ -20,30 +20,6 @@ class ModuleAdmin(VersionAdmin):
     list_display = ("id", "title", "description", "semester", "status", "archived", "type") #"get_all_disciplines",
     filter_horizontal = ("results", "competences")
     list_filter = ('archived', "semester", 'created', 'updated', 'type')
-
-
-@admin.register(GeneralBaseModulesPool)
-class GeneralBaseModulesPoolAdmin(VersionAdmin):
-    fields = ("title", "description", "modules", "status", "archived")
-    list_display = ("id", "title", "description", "get_all", "status", "archived")
-    list_filter = ('archived', 'created', 'updated')
-    filter_horizontal = ("modules", )
-
-
-@admin.register(EducationalProgramTrajectoriesPool)
-class EducationalProgramTrajectoriesPoolAdmin(VersionAdmin):
-    fields = ("title", "description", "modules", "status", "archived")
-    list_display = ("id", "title", "description", "get_all", "status", "archived")
-    list_filter = ('archived', 'created', 'updated')
-    filter_horizontal = ("modules", )
-
-
-@admin.register(ChoiceModulesPool)
-class ChoiceModulesPoolAdmin(VersionAdmin):
-    fields = ("title", "description", "modules", "status", "archived")
-    list_display = ("id", "title", "description", "get_all", "status", "archived")
-    list_filter = ('archived', 'created', 'updated')
-    filter_horizontal = ("modules", )
 
 
 @admin.register(Type)
