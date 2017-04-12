338c269d457ab811fdfc3b51827c176cc3112dd4
MasterGowen <mastergowen@gmail.com>
2017-04-13 02:24:47 +0500

    try from json_field import JSONField

--- open_programs/apps/cms/admin.py
+++ open_programs/apps/cms/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from jsoneditor.fields.django_jsonfield import JSONField
+from json_field import JSONField
 from jsoneditor.forms import JSONEditor
 from reversion.admin import VersionAdmin
 from codemirror2.widgets import CodeMirrorEditor
