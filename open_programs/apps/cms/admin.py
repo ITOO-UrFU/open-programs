fd1573cc52a9d1f59dd056b8450bc1b066309382
MasterGowen <mastergowen@gmail.com>
2017-04-13 01:28:18 +0500

    add django-tinymce to requirements

--- open_programs/apps/cms/admin.py
+++ open_programs/apps/cms/admin.py
@@ -2,10 +2,29 @@ from django.contrib import admin
 
 from jsonfield import JSONField
 from jsoneditor.forms import JSONEditor
+from reversion.admin import VersionAdmin
+from codemirror2.widgets import CodeMirrorEditor
+
+from .models import Component, ComponentType
+from .models import Container, ContainerType
 
 
+@admin.register(Component)
+class ComponentAdmin(VersionAdmin):
 
-class MyAdmin(admin.ModelAdmin):
+    def formfield_for_dbfield(self, db_field, **kwargs):
+        if db_field.attname in ("description", "about"):
+            kwargs['widget'] = CodeMirrorEditor(options={'mode': 'htmlmixed', 'lineNumbers': True, 'lineWrapping': True},
+                                                modes=['css', 'xml', 'javascript', 'htmlmixed'],
+                                                )
+        return super(ComponentAdmin, self).formfield_for_dbfield(db_field, **kwargs)
+
     formfield_overrides = {
-        JSONField:{'widget': JSONEditor},
+        JSONField: {'widget': JSONEditor},
     }
+
+    fields = ("title", "slug", "type",  "dev_description", "components", "weight", "status", "archived")
+    list_display = ("title", "slug", "weight", "status", "archived")
+    filter_horizontal = ("components", )
+    list_filter = ("slug", "weight", "status", "archived")
+    search_fields = ('slug', "title", "dev_description")
--- open_programs/apps/cms/models.py
+++ open_programs/apps/cms/models.py
@@ -3,6 +3,8 @@ from base.models import ObjectBaseClass
 from jsoneditor.fields.django_jsonfield import JSONField
 from django.utils.translation import ugettext_lazy as _
 import uuid
+from tinymce.models import HTMLField
+from open_programs.settings import TINYMCE_PROFILE
 
 
 def random_container_key():
@@ -19,7 +21,7 @@ def random_component_key():
 
 class ContainerType(models.Model):
     title = models.CharField(_("Название типа контейнера"), max_length=256, blank=False, default=random_container_key)
-    dev_description = models.TextField(_("Описание"), max_length=16384, blank=True, default="")  # TODO: WYSIWYG
+    dev_description = HTMLField()
 
     def __str__(self):
         return self.title
@@ -27,7 +29,7 @@ class ContainerType(models.Model):
 
 class ComponentType(models.Model):
     title = models.CharField(_("Название типа компонента"), max_length=256, blank=False, default=random_component_key)
-    dev_description = models.TextField(_("Описание"), max_length=16384, blank=True, default="")  # TODO: WYSIWYG
+    dev_description = HTMLField()
 
     def __str__(self):
         return self.title
@@ -36,10 +38,10 @@ class ComponentType(models.Model):
 class Container(ObjectBaseClass):
     title = models.CharField(_("Название контейнера"), max_length=256, blank=False, default=random_container_key)
     slug = models.SlugField(_("Код"))
-    dev_description = models.TextField(_("Описание"), max_length=16384, blank=True, default="")  # TODO: WYSIWYG
+    dev_description = HTMLField()
     type = models.ForeignKey("ContainerType", blank=True)
     weight = models.IntegerField(_("Вес"))
-    #  TODO: Container m2m
+    containers = models.ManyToManyField("self", verbose_name=_("Вложенные контейнеры"), blank=True, related_name='+', symmetrical=False)
 
     class Meta:
         verbose_name = 'контейнер'
@@ -52,12 +54,12 @@ class Container(ObjectBaseClass):
 class Component(ObjectBaseClass):
     title = models.CharField(_("Название компонента"), max_length=256, blank=False, default=random_component_key)
     slug = models.SlugField(_("Код"))
-    dev_description = models.TextField(_("Описание"), max_length=16384, blank=True, default="")  # TODO: WYSIWYG
+    dev_description = HTMLField()
     type = models.ForeignKey("ComponentType", blank=True)
     weight = models.IntegerField(_("Вес"))
     content = models.TextField(_("Контент"), blank=True, null=True)
     json = JSONField()
-    # TODO:  component m2m
+    components = models.ManyToManyField("self", verbose_name=_("Вложенные компоненты"), blank=True, related_name='+', symmetrical=False)
 
     class Meta:
         verbose_name = 'компонент'
