8470233b825e7a0ef7a84074c0098a6e9212b6a5
MasterGowen <mastergowen@gmail.com>
2017-04-13 10:33:32 +0500

    countries db hotfix

--- open_programs/apps/persons/models.py
+++ open_programs/apps/persons/models.py
@@ -21,7 +21,7 @@ class Person(models.Model):
                            default='U'
                            )
     alt_email = models.EmailField(_('Альтернативный e-mail'), max_length=254, blank=True)
-    country = CountryField(blank=True, default='Russia')
+    country = CountryField(blank=True, default='RU')
     birthday_date = models.DateField(_('Дата рождения'), null=True, blank=True)
 #   roles = models.ManyToManyField(ROLE)  TODO: Сделать модель 'Роль'
     biography = models.TextField(_('Биография пользователя'), blank=True, default='')
