fb161982f923bedbb11a0426a58a1d2b240b9584
MasterGowen <mastergowen@gmail.com>
2017-04-14 19:32:39 +0500

    ContainerListByType#6

--- open_programs/apps/cms/serializers.py
+++ open_programs/apps/cms/serializers.py
@@ -15,12 +15,14 @@ class ComponentTypeSerializer(serializers.ModelSerializer):
 
 
 class ComponentSerializer(serializers.ModelSerializer):
+    get_type = serializers.Field()
     class Meta:
         model = Component
         fields = ("id", "title", "slug", "dev_description", "get_type", "weight", "content", "json")
 
 
 class SubContainerSerializer(serializers.ModelSerializer):
+    get_type = serializers.Field()
     components = ComponentSerializer(
         many=True,
         read_only=True
@@ -33,7 +35,7 @@ class SubContainerSerializer(serializers.ModelSerializer):
 
 
 class ContainerSerializer(serializers.ModelSerializer):
-
+    get_type = serializers.Field()
     containers = SubContainerSerializer(
         many=True,
         read_only=True
