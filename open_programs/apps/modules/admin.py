cd75c8cd41ece073ce3ebb470e621a38bd90a9d7
MasterGowen <mastergowen@gmail.com>
2017-04-20 16:35:09 +0500

    discipline#9

--- open_programs/apps/modules/admin.py
+++ open_programs/apps/modules/admin.py
@@ -6,17 +6,6 @@ from .models import Module, Type
 
 @admin.register(Module)
 class ModuleAdmin(VersionAdmin):
-    fields = ("title",
-              "description",
-              "semester",
-              "status",
-              "archived",
-              "type",
-              "results",
-              "results_text",
-              "competences"
-              )
-
     list_display = ("id", "title", "description", "semester", "status", "archived", "type") #"get_all_disciplines",
     filter_horizontal = ("results", "competences")
     list_filter = ('archived', "semester", 'created', 'updated', 'type')
