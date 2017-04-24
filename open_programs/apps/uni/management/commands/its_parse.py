74fef2cfae3a60d84b0876e922f49a0c028f395a
MasterGowen <mastergowen@gmail.com>
2017-04-24 17:36:11 +0500

    its#1

---
+++ open_programs/apps/uni/management/commands/its_parse.py
@@ -0,0 +1,30 @@
+from bs4 import BeautifulSoup
+import re
+import json
+
+from django.core.management.base import BaseCommand
+
+from programs.models import Program, ProgramModules, LearningPlan
+from disciplines.models import Discipline, Semester, TrainingTerms
+from modules.models import Module
+
+
+class Command(BaseCommand):
+    help = "Create Django objects from raw&ugly UrFU data."
+    requires_system_checks = True
+    requires_migrations_checks = True
+
+    def add_arguments(self, parser):
+        parser.add_argument('un', nargs=1)
+
+    def handle(self, *args, **options):
+        try:
+            oksos = []
+            with open('uni_fixtures/specialities.json', encoding='utf-8') as specialities:
+                specialities_json = json.load(specialities)
+                for speciality in specialities:
+                    oksos.append(speciality["okso"])
+        except:
+            raise FileNotFoundError
+        print(oksos)
+
