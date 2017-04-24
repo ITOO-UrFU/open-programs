5b1a48b79748f87b64cc1a6e47f51d6f86fc7da6
MasterGowen <mastergowen@gmail.com>
2017-04-25 01:35:25 +0500

    its_get#6

--- open_programs/apps/uni/management/commands/its_get.py
+++ open_programs/apps/uni/management/commands/its_get.py
@@ -39,6 +39,7 @@ class Command(BaseCommand):
                     [oksos.append(s["okso"]) for s in specialities_json]
                 print(f"{bcolors.OKGREEN}Всего ОКСО: {len(list(set(oksos)))}{bcolors.ENDC}")
                 self.urls = [f"http://its.urfu.ru/api/programs?okso={okso}" for okso in list(set(oksos))]
+                print(list(set(oksos)))
 
             def exception(self, request, exception):
                 print(f"{bcolors.FAIL}Problem: {request.url}: {exception}{bcolors.ENDC}")
