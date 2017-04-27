4fc875d8578b811d8286b8d6fb39bc0eb76cdff8
MasterGowen <mastergowen@gmail.com>
2017-04-27 19:47:26 +0500

    result ser#3 , read_only=True

--- open_programs/apps/competences/serializers.py
+++ open_programs/apps/competences/serializers.py
@@ -4,7 +4,7 @@ from rest_framework import serializers
 
 
 class CompetenceSerializer(serializers.HyperlinkedModelSerializer):
-    results = ResultSerializer(many=True)
+    results = ResultSerializer(many=True, read_only=True)
     class Meta:
         model = Competence
         fields = ("title", "results", "status", "archived", "created", "updated")
