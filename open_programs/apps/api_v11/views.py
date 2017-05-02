71bc79566e7f1caffef586ba93587cb1d139ba35
MasterGowen <mastergowen@gmail.com>
2017-05-02 19:23:17 +0500

    targets_positions#2

--- open_programs/apps/api_v11/views.py
+++ open_programs/apps/api_v11/views.py
@@ -231,6 +231,7 @@ def get_program_modules(request, program_id):
             targets_positions = []
             try:
                 tr_targets = TrainingTarget.objects.filter(program__id=program_id).order_by('-number')
+                print(tr_targets, '!!!!!!!!!!!!!!!!!!!!')
                 for tt in tr_targets:
                     tms = TargetModules.objects.filter(program__id=program_id, program_module=pr_mod, target=tt, status="p",
                                                        archived=False)
