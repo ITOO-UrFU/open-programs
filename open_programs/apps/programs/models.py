79d71adfd0fc544902a1507756f2a58ab25d6a22
MasterGowen <mastergowen@gmail.com>
2017-05-02 16:34:21 +0500

    get_targets_by_program#1

--- open_programs/apps/programs/models.py
+++ open_programs/apps/programs/models.py
@@ -60,7 +60,7 @@ class Program(ObjectBaseClass):
 class TrainingTarget(ObjectBaseClass):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(_('Наименование образовательной цели'), blank=False, max_length=256, default=_(''))
-    program = models.ManyToManyField("Program")
+    program = models.ForeignKey("Program")
     number = models.IntegerField(_("Порядковый номер цели"))
 
     class Meta:
