bef611a60808e339d4de7d9a3cd0c09bfd2dc7de
MasterGowen <mastergowen@gmail.com>
2017-05-04 18:56:02 +0500

    sort cheat#3 test

--- open_programs/apps/api_v11/views.py
+++ open_programs/apps/api_v11/views.py
@@ -246,7 +246,8 @@ def get_program_modules(request, program_id):
                     "choice_group": None if not mod.choice_group else mod.choice_group.id,
                     "targets_positions": mod.get_target_positions(),
                     })
-    return Response(sorted(response, key=lambda k: (k['weight'], k["title"])))
+    # return Response(sorted(response, key=lambda k: (k['weight'], k["title"])))
+    return Response(response)
 
 
 @api_view(("POST", ))
