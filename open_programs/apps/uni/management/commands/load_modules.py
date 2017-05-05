7278add7d775c070a1387b94d9420d9dee3f70a1
MasterGowen <mastergowen@gmail.com>
2017-05-05 12:09:56 +0500

    priority from uni db

---
+++ open_programs/apps/uni/management/commands/load_modules.py
@@ -0,0 +1,31 @@
+from django.core.management.base import BaseCommand
+import os
+import json
+from django.conf import settings
+from modules.models import Module
+
+
+class Command(BaseCommand):
+    help = "Загрузка справочника модулей"
+
+    def handle(self, *args, **options):
+        def update_if_none(m, field, json_val=None):
+            if json_val is None:
+                return False
+            if getattr(m, field) is None:
+                m.__dict__[field] = json_val
+                return True
+
+        with open(os.path.join(settings.BASE_DIR, "uni_fixtures", "modules.json"), encoding='utf8') as basefile:
+            fixtures = json.load(basefile)
+            for module in fixtures:
+                m = Module.objects.filter(title=module["title"]).first()
+                if m:
+                    m.uni_uuid = module["uuid"]
+                m = Module(uid=speciality["uid"],
+                                  okso=speciality["okso"],
+                                  title=speciality["title"],
+                                  ministerialCode=speciality["ministerialCode"],
+                                  ugnTitle=speciality["ugnTitle"],
+                                  standard=speciality["standard"])
+                spec.save()
