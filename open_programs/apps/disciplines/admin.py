f8770fc0a346f8847a61d3b33fea4525f9120571
MasterGowen <mastergowen@gmail.com>
2017-05-12 12:50:07 +0500

    ajax_select#6

--- open_programs/apps/disciplines/admin.py
+++ open_programs/apps/disciplines/admin.py
@@ -30,4 +30,4 @@ class VariantAdmin(VersionAdmin, AjaxSelectAdmin):
     list_display = ("discipline", "program", "course", "semester", "parity", "link", "status", "archived")
     list_filter = ("parity", "archived", "created", "updated", "status")
     search_fields = ("discipline__title", "program__title", "course__title", "parity", "link")
-    form = make_ajax_form(Variant, {'discipline': 'discipline'})
+    form = make_ajax_form(Variant, {'discipline': 'discipline', "semester": "semester"})
--- open_programs/apps/disciplines/lookups.py
+++ open_programs/apps/disciplines/lookups.py
@@ -23,3 +23,23 @@ class DisciplineLookup(LookupChannel):
     def format_item_display(self, obj):
         """ (HTML) formatted item for displaying item in the selected deck area """
         return escape(str(obj))
+
+
+class SemesterLookup(LookupChannel):
+
+    model = Semester
+
+    def get_query(self, q, request):
+        return Semester.objects.filter(Q(discipline__title__icontains=q) | Q(discipline__uni_number__icontains=q) | Q(discipline__uni_uid__icontains=q))
+
+    def get_result(self, obj):
+        """ result is the simple text that is the completion of what the person typed """
+        return str(obj)
+
+    def format_match(self, obj):
+        """ (HTML) formatted item for display in the dropdown """
+        return escape(f"{obj.discipline.title} - {obj.term}")
+
+    def format_item_display(self, obj):
+        """ (HTML) formatted item for displaying item in the selected deck area """
+        return escape(f"{obj.discipline.title} - {obj.term}")
