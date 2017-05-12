b3f5e214f4caab574c21e4c71da55c1092da83e7
MasterGowen <mastergowen@gmail.com>
2017-05-12 12:41:50 +0500

    ajax_select#4

--- open_programs/apps/disciplines/admin.py
+++ open_programs/apps/disciplines/admin.py
@@ -8,9 +8,9 @@ from .models import Discipline, TrainingTerms, Semester, Variant
 
 @admin.register(Discipline)
 class DisciplineAdmin(VersionAdmin):
-    fields = ("title", "description", "courses", "module", "period",  "labor", "form", "status", "archived", "results", "results_text")
+    fields = ("title", "description", "module", "period",  "labor", "form", "status", "archived", "results", "results_text")
     list_display = ("id", "title", "description", "module", "get_all", "period", "labor", "form", "status", "archived")
-    filter_horizontal = ("courses", "results")
+    filter_horizontal = ( "results")
     list_filter = ("archived", "created", "updated", "status", "form")
     search_fields = ['title', 'module__title', "module__uni_number"]
 
