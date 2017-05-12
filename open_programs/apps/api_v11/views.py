12761e8e3e2b5f8384d7d5befa91ee26e7fd6327
MasterGowen <mastergowen@gmail.com>
2017-05-12 19:31:18 +0500

    cache#14 uff

--- open_programs/apps/api_v11/views.py
+++ open_programs/apps/api_v11/views.py
@@ -233,8 +233,6 @@ def get_competences_by_program(request, program_id):
 
 @api_view(('GET',))
 def get_program_modules(request, program_id):
-    import time
-    start_time = time.time()
     trigger = Changed.objects.filter(program__id=program_id).first()
     if not trigger:
         trigger = Changed.objects.create(program=Program.objects.get(id=program_id))
@@ -242,10 +240,8 @@ def get_program_modules(request, program_id):
         trigger.save()
     if not trigger.state():
         return Response(cache.get(f"gpm-{program_id}"))
-    print(f"{(start_time - time.time())} - TRIGGER")
     response = []
     for mod in ProgramModules.objects.filter(program__id=program_id, status="p", archived=False):
-        start_time = time.time()
         response.append({
                     "id": mod.id,
                     "title": mod.module.title,
@@ -261,7 +257,6 @@ def get_program_modules(request, program_id):
                     "targets_positions": mod.get_target_positions(),
                     "priority": 9999 if not mod.module.uni_priority else mod.module.uni_priority
                     })
-        print(f"{(start_time - time.time())} - RESPONSE")
     response = sorted(response, key=lambda k: (k["semester"], k["priority"], k["title"]))
     cache.set(f"gpm-{program_id}", response, 3600)
     trigger.deactivate()
