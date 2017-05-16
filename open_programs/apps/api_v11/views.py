89e186da67987312675ec384b697aec003524270
MasterGowen <mastergowen@gmail.com>
2017-05-16 14:20:52 +0500

    change_discipline_semester#14

--- open_programs/apps/api_v11/views.py
+++ open_programs/apps/api_v11/views.py
@@ -426,9 +426,10 @@ def change_discipline_semester(request):
     term_title = request.data["term_title"]
     new_semester = request.data["semester"]
 
-    try:
-        Semester.objects.filter(program=program, discipline=discipline, term__title=term_title).update(training_semester=new_semester)
-    except:
+    semester = Semester.objects.filter(program=program, discipline=discipline, term__title=term_title)
+    if semester:
+        semester.update(training_semester=new_semester)
+    else:
         Semester.objects.create(program=program, discipline=discipline, term=TrainingTerms.objects.filter(title=term_title).first(), training_semester=new_semester)
     return Response(status=200)
 
