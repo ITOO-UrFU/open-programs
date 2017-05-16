84b7b1292c0c86c8892e322a1b6401e42266caf4
MasterGowen <mastergowen@gmail.com>
2017-05-16 15:45:16 +0500

    slickgrid#1

--- open_programs/apps/disciplines/admin.py
+++ open_programs/apps/disciplines/admin.py
@@ -3,10 +3,76 @@ from reversion.admin import VersionAdmin
 from ajax_select.admin import AjaxSelectAdmin
 from ajax_select import make_ajax_form
 
-from cms.admin import JSONEditor
+# from cms.admin import JSONEditor
+
+from django.forms.widgets import Textarea
+try:
+    from django.forms.util import flatatt
+except ImportError:
+    from django.forms.utils import flatatt
+from django.utils.safestring import mark_safe
+from django.conf import settings
+import json
+
 from jsonfield import JSONField
 from .models import Discipline, TrainingTerms, Semester, Variant, Technology, Diagram
 
+
+class JSONEditor(Textarea):
+    class Media:
+        js = (
+            getattr(settings, "SLICK_JS_CORE", settings.STATIC_URL+'slickgrid/slick.core.js'),
+            getattr(settings, "SLICK_JS_FORMATTERS", settings.STATIC_URL + 'slickgrid/slick.formatters.js'),
+            getattr(settings, "SLICK_JS_EDITOR", settings.STATIC_URL + 'slickgrid/slick.editor.js'),
+            getattr(settings, "SLICK_JS_GRID", settings.STATIC_URL + 'slickgrid/slick.grid.js'),
+        )
+        css = {'all': (getattr(settings, "SLICK_CSS", settings.STATIC_URL+'slickgrid/slick.grid.css'),)}
+
+    def render(self, name, value, attrs=None):
+        try:
+            value = json.loads(value)
+        except TypeError:
+            pass
+        input_attrs = {'hidden': True}
+        input_attrs.update(attrs)
+        if 'class' not in input_attrs:
+            input_attrs['class'] = 'for_jsoneditor'
+        else:
+            input_attrs['class'] += ' for_jsoneditor'
+        r = super(JSONEditor, self).render(name, value, input_attrs)
+        div_attrs = {}
+        div_attrs.update(attrs)
+        div_attrs.update({'id': (attrs['id']+'_jsoneditor')})
+        final_attrs = self.build_attrs(div_attrs, extra_attrs={"name": name})
+        r += '''
+        <style type="text/css">
+            #id_%(name)s_jsoneditor {
+              height: 400px;
+              margin-bottom: 1em;
+            }
+          </style>
+        <script>
+
+        document.addEventListener("DOMContentLoaded", function(event) {
+            var jsoncontainer = document.getElementById("id_%(name)s_jsoneditor");
+            var options = {
+                mode: 'code',
+                modes: ['code', 'tree'],
+                onChange: function(){document.getElementById("id_%(name)s").value = JSON.stringify(editor.get())}
+            };
+            var editor = new JSONEditor(jsoncontainer, options);
+
+            editor.set(JSON.parse(document.getElementById("id_%(name)s").value.replace(/'/g, '"')));
+            document.getElementById("id_%(name)s").value = JSON.stringify(editor.get());
+        });
+
+        </script>
+        <div %(attrs)s></div>
+        ''' % {
+            'attrs': flatatt(final_attrs),
+            'name': name
+        }
+        return mark_safe(r)
 
 @admin.register(Discipline)
 class DisciplineAdmin(VersionAdmin):
