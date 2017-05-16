fc1d91156ea79e2ec0d18b34ee46185f5e192766
MasterGowen <mastergowen@gmail.com>
2017-05-16 14:05:39 +0500

    change_discipline_semester#6

--- open_programs/apps/disciplines/admin.py
+++ open_programs/apps/disciplines/admin.py
@@ -25,6 +25,7 @@ class TrainingTermsAdmin(VersionAdmin):
 @admin.register(Semester)
 class SemesterAdmin(VersionAdmin):
     list_display = ("__str__", )
+    search_fields = ("discipline__title", "discipline__module__title", "program__title")
 
 
 @admin.register(Variant)
