53c73c705e7d20ad0fc7cf892882dbb0a68eb58b
MasterGowen <mastergowen@gmail.com>
2017-05-18 17:09:57 +0500

    add corse addr

--- open_programs/settings.py
+++ open_programs/settings.py
@@ -254,9 +254,7 @@ REST_FRAMEWORK = {
 
 #### CORS ####
 CORS_ORIGIN_WHITELIST = (
-    'localhost:8000',
-    '127.0.0.1:8000',
-    'localhost:3000',
+    '192.168.0.100:4200',
     'localhost:4200',
     '127.0.0.1:4200',
     "http://openprograms.ru",
