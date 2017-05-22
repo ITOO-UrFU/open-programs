29e62d2da1a2c3cf341ff4c415f3518df851b8a1
MasterGowen <mastergowen@gmail.com>
2017-05-22 20:00:23 +0500

    cache exercises#0

--- open_programs/apps/api_v11/views.py
+++ open_programs/apps/api_v11/views.py
@@ -38,6 +38,27 @@ class ProgramList(viewsets.ModelViewSet):
     queryset = Program.objects.filter(status="p", archived=False)
     serializer_class = ProgramSerializer
 
+    def get_object(self):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        # Perform the lookup filtering.
+        lookup_url_kwarg = self.lookup_url_kwarg or self.lookup_field
+
+        assert lookup_url_kwarg in self.kwargs, (
+            'Expected view %s to be called with a URL keyword argument '
+            'named "%s". Fix your URL conf, or set the `.lookup_field` '
+            'attribute on the view correctly.' %
+            (self.__class__.__name__, lookup_url_kwarg)
+        )
+
+        filter_kwargs = {self.lookup_field: self.kwargs[lookup_url_kwarg]}
+        obj = get_object_or_404(queryset, **filter_kwargs)
+
+        # May raise a permission denied
+        self.check_object_permissions(self.request, obj)
+
+        return obj
+
 
 class ModuleList(viewsets.ModelViewSet):
     queryset = Module.objects.filter(status="p", archived=False)
