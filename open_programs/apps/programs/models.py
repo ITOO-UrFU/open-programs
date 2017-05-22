5356d179cf38f7eb4b2e6534d79eeb02ee572691
MasterGowen <mastergowen@gmail.com>
2017-05-22 16:48:46 +0500

    competence diagram#13

--- open_programs/apps/programs/models.py
+++ open_programs/apps/programs/models.py
@@ -67,7 +67,7 @@ class Program(ObjectBaseClass):
             response[target.title] = []
             for competence in ProgramCompetence.objects.filter(program=self):
                 labors = [pm.module.get_labor() for pm in
-                    ProgramModules.objects.filter(program=self, competence=self, status="p", archived=False, module__id__in=target.get_mandatory_modules_id())]
+                    ProgramModules.objects.filter(program=self, competence=competence, status="p", archived=False, module__id__in=target.get_mandatory_modules_id())]
                 response[target.title].append(([competence.title, competence.color, sum([0 if not labor else labor for labor in labors])]))
 
         return response
