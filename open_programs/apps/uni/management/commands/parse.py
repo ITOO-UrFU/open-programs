64356df0c9577608d4d1c8175e7a636f185f1cd6
MasterGowen <mastergowen@gmail.com>
2017-06-02 14:35:16 +0500

    parser#7

--- open_programs/apps/uni/management/commands/parse.py
+++ open_programs/apps/uni/management/commands/parse.py
@@ -273,12 +273,12 @@ class Command(BaseCommand):
                         break
                 try:
                     print(f"{self.bcolors.BOLD}Ищем дисциплину \"{d['title']}\" модуля \"{module_obj.title}\"!{self.bcolors.ENDC}")
-                    discipline = Discipline.objects.get(title=d["title"].strip())
+                    discipline = Discipline.objects.get(title=d["title"], module=module_obj)
                     if discipline:
                         print(f"{self.bcolors.OKGREEN}Существует дисциплина {discipline.title}!{self.bcolors.ENDC}")
                 except:
                     print(f"{self.bcolors.FAIL}Не существует дисциплины {d['title']}!!{self.bcolors.ENDC}")
-                    discipline = Discipline(title=d["title"], module=module_obj)
+                    discipline = Discipline(title=d["title"])
 
                 for i in range(10, 0, -1):
                     try:
