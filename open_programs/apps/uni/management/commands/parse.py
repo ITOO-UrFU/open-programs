55752c78679ac027dcf1c491c226cbc075579d3f
MasterGowen <mastergowen@gmail.com>
2017-06-02 15:36:43 +0500

    parser#21

--- open_programs/apps/uni/management/commands/parse.py
+++ open_programs/apps/uni/management/commands/parse.py
@@ -272,17 +272,15 @@ class Command(BaseCommand):
                     if d["title"] in row:
                         break
 
-
-                try:
-                    print(
-                        f"{self.bcolors.BOLD}Ищем дисциплину \"{d['title']}\" модуля \"{module_obj.title}\"!{self.bcolors.ENDC}")
-                    discipline = Discipline.objects.filter(title=d["title"],
-                                                           module__in=Module.objects.filter(uni_uuid=module["uuid"]),
-                                                           module__program=program).first()
-                    print(discipline)
-                    if discipline:
-                        print(f"{self.bcolors.OKGREEN}Существует дисциплина {discipline.title}!{self.bcolors.ENDC}")
-                except:
+                print(
+                    f"{self.bcolors.BOLD}Ищем дисциплину \"{d['title']}\" модуля \"{module_obj.title}\"!{self.bcolors.ENDC}")
+                discipline = Discipline.objects.filter(title=d["title"],
+                                                       module__in=Module.objects.filter(uni_uuid=module["uuid"]),
+                                                       module__program=program).first()
+                print(discipline)
+                if discipline:
+                    print(f"{self.bcolors.OKGREEN}Существует дисциплина {discipline.title}!{self.bcolors.ENDC}")
+                else:
                     print(f"{self.bcolors.FAIL}Не существует дисциплины {d['title']}!!{self.bcolors.ENDC}")
                     discipline = Discipline(title=d["title"])
 
