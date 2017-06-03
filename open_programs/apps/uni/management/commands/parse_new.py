4ab3a6447894a2929b6b45c49ded3ddeec9f1534
MasterGowen <mastergowen@gmail.com>
2017-06-03 17:01:56 +0500

    new parser#31

--- open_programs/apps/uni/management/commands/parse_new.py
+++ open_programs/apps/uni/management/commands/parse_new.py
@@ -205,7 +205,7 @@ class Command(BaseCommand):
                 for module in [m for m in modules if m["disciplines"]]:
                     module_obj, semester = self.create_module(find_row_index_id, module, program)
 
-        program_modules_fail = ProgramModules.objects.filter(~Q(id__in=[o.id for o in program_modules]) & Q(program=program))
+        program_modules_fail = ProgramModules.objects.filter(~Q(id__in=[o.id for o in program_modules]), Q(program=program))
         print(program_modules_fail)
         for pmf in program_modules_fail:
             remove = input(f"{self.bcolors.WARNING}Неверный модуль программы: {pmf.module.title}. Удалить?{self.bcolors.ENDC}")
