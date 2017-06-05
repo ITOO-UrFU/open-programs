897ee5ca8694ffc5a79aa63429a4840302174ea6
MasterGowen <mastergowen@gmail.com>
2017-06-05 09:23:23 +0500

    new parser#53

--- open_programs/apps/uni/management/commands/parse_new.py
+++ open_programs/apps/uni/management/commands/parse_new.py
@@ -346,44 +346,44 @@ class Command(BaseCommand):
                         pass
                 print(semesters)
 
-                print(
-                    f"{self.bcolors.BOLD}Ищем дисциплину \"{d['title']}\" модуля \"{module_obj.title}\"!{self.bcolors.ENDC}")
-                discipline = Discipline.objects.filter(title=d["title"],
-                                                       module__in=Module.objects.filter(uni_uuid=module["uuid"]),
-                                                       module__program=program).first()
-                print(discipline)
-                if discipline:
-                    print(f"{self.bcolors.OKGREEN}Существует дисциплина {discipline.title}!{self.bcolors.ENDC}")
-                else:
-                    print(f"{self.bcolors.FAIL}Не существует дисциплины {d['title']}!!{self.bcolors.ENDC}")
-                    discipline = Discipline(title=d["title"])
-
-
-
-                discipline.module = module_obj
-                discipline.labor = d["testUnits"]
-                discipline.uni_uid = d["uid"]
-                discipline.uni_discipline = d["discipline"]
-                discipline.uni_number = d["number"]
-                discipline.uni_section = d["section"]
-                discipline.uni_file = d["file"]
-                discipline.period = semester - module_obj.semester + 1
-                try:
-                    try:
-                        if int(max(row[5].split("-"))):
-                            discipline.form = "z"
-                    except:
-                        pass
-                    try:
-                        if int(max(row[4].split("-"))):
-                            discipline.form = "e"
-                    except:
-                        pass
-                except:
-                    pass
-
-                discipline.status = "p"
-                # discipline.save()
-                # self.create_semester(program, discipline, module, find_row_index_id, term)
-                print(f"{self.bcolors.OKBLUE}{discipline.title}{self.bcolors.ENDC}")
+                # print(
+                #     f"{self.bcolors.BOLD}Ищем дисциплину \"{d['title']}\" модуля \"{module_obj.title}\"!{self.bcolors.ENDC}")
+                # discipline = Discipline.objects.filter(title=d["title"],
+                #                                        module__in=Module.objects.filter(uni_uuid=module["uuid"]),
+                #                                        module__program=program).first()
+                # print(discipline)
+                # if discipline:
+                #     print(f"{self.bcolors.OKGREEN}Существует дисциплина {discipline.title}!{self.bcolors.ENDC}")
+                # else:
+                #     print(f"{self.bcolors.FAIL}Не существует дисциплины {d['title']}!!{self.bcolors.ENDC}")
+                #     discipline = Discipline(title=d["title"])
+                #
+                #
+                #
+                # discipline.module = module_obj
+                # discipline.labor = d["testUnits"]
+                # discipline.uni_uid = d["uid"]
+                # discipline.uni_discipline = d["discipline"]
+                # discipline.uni_number = d["number"]
+                # discipline.uni_section = d["section"]
+                # discipline.uni_file = d["file"]
+                # discipline.period = semester - module_obj.semester + 1
+                # try:
+                #     try:
+                #         if int(max(row[5].split("-"))):
+                #             discipline.form = "z"
+                #     except:
+                #         pass
+                #     try:
+                #         if int(max(row[4].split("-"))):
+                #             discipline.form = "e"
+                #     except:
+                #         pass
+                # except:
+                #     pass
+                #
+                # discipline.status = "p"
+                # # discipline.save()
+                # # self.create_semester(program, discipline, module, find_row_index_id, term)
+                # print(f"{self.bcolors.OKBLUE}{discipline.title}{self.bcolors.ENDC}")
         return semesters
