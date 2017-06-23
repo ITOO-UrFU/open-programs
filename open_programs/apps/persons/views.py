9237824c42062df6e6a8d3f9eeb7fc6ffe1e1e2c
MasterGowen <mastergowen@gmail.com>
2017-06-23 19:02:07 +0500

    register#2

--- open_programs/apps/persons/views.py
+++ open_programs/apps/persons/views.py
@@ -2,11 +2,13 @@ from rest_framework.decorators import api_view, permission_classes
 from .serializers import UserSerializer
 from django.contrib.auth import authenticate, get_user_model
 from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
 
 User = get_user_model()
 
 
 @api_view(['POST'])
+@permission_classes((AllowAny, ))
 def register(request):
     serialized = UserSerializer(data={
         "username": request.DATA.get("username", None),
