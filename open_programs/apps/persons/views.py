109f95f64fff11c2a41459cb185e0e26f908578e
MasterGowen <mastergowen@gmail.com>
2017-06-24 17:21:51 +0500

    register#23

--- open_programs/apps/persons/views.py
+++ open_programs/apps/persons/views.py
@@ -1,12 +1,22 @@
 from rest_framework.decorators import api_view, permission_classes
 from .serializers import UserSerializer, PersonSerializer
+
 from django.contrib.auth import authenticate, get_user_model
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 
 from .models import Person
 
 User = get_user_model()
+
+
+@receiver(post_save, sender=User)
+def create_favorites(sender, instance, created, **kwargs):
+    if created:
+        Person.objects.create(user=instance)
 
 
 @api_view(['POST'])
@@ -25,21 +35,19 @@ def register(request):
         )
         user.save()
 
-        if user:
-            print(user, "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-            # person = Person(
-            #     user=user,
-            #     first_name=request.data.get("first_name", ""),
-            #     last_name=request.data.get("last_name", ""),
-            #     second_name=request.data.get("second_name", ""),
-            #     sex=request.data.get("sex", 'U'),
-            #     alt_email=request.data.get("alt_email", ""),
-            #     birthday_date=request.data.get("birthday_date", None),
-            #     biography=request.data.get("biography", ""),
-            # )
-            # person.save()
+        person = Person.objects.filter(user=user).first()
+        if person:
+            person.first_name = request.data.get("first_name", ""),
+            person.last_name = request.data.get("last_name", ""),
+            person.second_name = request.data.get("second_name", ""),
+            person.sex = request.data.get("sex", 'U'),
+            person.alt_email = request.data.get("alt_email", ""),
+            person.birthday_date = request.data.get("birthday_date", None),
+            person.biography = request.data.get("biography", ""),
 
-            person = "PersonSerializer(person)"
+            person.save()
+
+            person = PersonSerializer(person)
 
             return Response(person, status=201)
     else:
