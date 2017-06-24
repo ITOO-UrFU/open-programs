f7c2ff1005b0d014bb3a21120d0096503cc82f19
MasterGowen <mastergowen@gmail.com>
2017-06-24 18:24:02 +0500

    permissions get#3

--- open_programs/apps/persons/serializers.py
+++ open_programs/apps/persons/serializers.py
@@ -1,12 +1,20 @@
 from .models import Person
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 
 
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = ('name',)
+
+
 class UserSerializer(serializers.HyperlinkedModelSerializer):
+    groups = GroupSerializer(many=True)
+
     class Meta:
         model = User
-        fields = ("id", "username", "email", "is_staff", "get_group_permissions")
+        fields = ("id", "username", "email", "is_staff", "groups")
 
 
 class PersonSerializer(serializers.HyperlinkedModelSerializer):
