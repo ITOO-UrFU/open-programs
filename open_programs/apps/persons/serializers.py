29fe5e073e3d8220af791c41ab1cf59fadacc705
MasterGowen <mastergowen@gmail.com>
2017-06-24 18:18:27 +0500

    permissions get#1

--- open_programs/apps/persons/serializers.py
+++ open_programs/apps/persons/serializers.py
@@ -6,7 +6,7 @@ from rest_framework import serializers
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ("id", "username", "email", "is_staff", "is_superuser")
+        fields = ("id", "username", "email", "is_staff", "is_superuser", "groups")
 
 
 class PersonSerializer(serializers.HyperlinkedModelSerializer):
