d14ed56010c368900209d7dea95f94d5dfe593db
MasterGowen <mastergowen@gmail.com>
2017-06-29 15:01:50 +0500

    is_manager decorator#10

--- open_programs/apps/api_v11/views.py
+++ open_programs/apps/api_v11/views.py
@@ -40,7 +40,7 @@ from django.http import HttpResponseForbidden
 from django.utils.decorators import available_attrs
 import logging
 
-def is_manager():
+def is_manager(f):
     def decorator(func):
         @wraps(func, assigned=available_attrs(func))
         def inner(request, *args, **kwargs):
