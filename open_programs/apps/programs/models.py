0342da8e3fd7986cc531fc15084bca04595897d1
MasterGowen <mastergowen@gmail.com>
2017-07-12 17:09:13 +0500

    default semester#13

--- open_programs/apps/programs/models.py
+++ open_programs/apps/programs/models.py
@@ -143,8 +143,9 @@ class ProgramModules(ObjectBaseClass):
 
     def get_all_discipline_custom(self):
         q = [[s for s in Semester.objects.filter(discipline=discipline, program=self.program)]for discipline in Discipline.objects.filter(module=self.module, archived=False, status="p").order_by("period")]
-        for s in q:
-            print(s.term.title, s.training_semester)
+        for sem in q:
+            for s in sem:
+                print(s.term.title, s.training_semester)
         print(q)
         return [{"id": discipline.id,
                  "title": discipline.title,
