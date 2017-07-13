0e86514fb8d04960bbfcb56dd7fd19687c9f59ec
MasterGowen <mastergowen@gmail.com>
2017-07-13 15:44:27 +0500

    presence#2

--- open_programs/apps/api_v11/views.py
+++ open_programs/apps/api_v11/views.py
@@ -691,13 +691,19 @@ def get_program_variants(request, program_id):
     for discipline in disciplines:
         variants[discipline.id] = []
         for variant in Variant.objects.filter(program=program, discipline__id=discipline.id):
-            if variant.technology:
-                if 'заоч' in variant.technology.title or 'истанционн' in variant.technology.title:
+            if variant.diagram:
+                if 'заоч' in variant.diagram.title:
                     presence = "z"
                 elif 'лайн' in variant.technology.title:
                     presence = 'online'
                 else:
                     presence = "o"
+
+                if "традиционная" in variant.technology.title.lower():
+                    technology_type = "t"
+                else:
+                    technology_type = "d"
+
             variants[discipline.id].append(
                 {
                     "id": variant.id,
@@ -719,6 +725,7 @@ def get_program_variants(request, program_id):
                         "contact_work_category": variant.technology.contact_work_category,
                         "color": variant.technology.color,
                         "presence": presence,
+                        "technology_type": technology_type,
 
                     },
                     "semester": None if not variant.semester else
