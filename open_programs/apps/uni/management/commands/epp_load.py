3bc0c6b01513ac47c7166aa3ea0e9388be873817
MasterGowen <mastergowen@gmail.com>
2017-08-10 14:54:41 +0500

    epp load#19

--- open_programs/apps/uni/management/commands/epp_load.py
+++ open_programs/apps/uni/management/commands/epp_load.py
@@ -108,8 +108,8 @@ class Command(BaseCommand):
 
             for discipline in Discipline.objects.filter(module=module_obj):
                 epp_discipline = [d for d in epp_module["disciplines"] if discipline.title in d['titleheaderCell']]
-                training_semester = epp_discipline["firstSemester"]
-                print(epp_discipline, training_semester)
+                # training_semester = epp_discipline["firstSemester"]
+                print(epp_discipline)  # , training_semester)
                 # semester_obj = Semester(discipline=discipline,
                 #                         training_semester=training_semester,
                 #                         program=program,
