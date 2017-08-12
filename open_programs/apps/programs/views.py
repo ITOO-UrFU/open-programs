d13609d57b3d55c251dbde47b5c4ac22be190899
MasterGowen <mastergowen@gmail.com>
2017-08-12 16:13:35 +0500

    program backup#3

--- open_programs/apps/programs/views.py
+++ open_programs/apps/programs/views.py
@@ -1,11 +1,38 @@
+from rest_framework import serializers
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .models import Program, TrainingTarget
+from .models import Program, TrainingTarget, ProgramModules, ChoiceGroup, ProgramCompetence
+
+
+class ProgramCompetenceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProgramCompetence
+        fields = ("id", "title", "number")
+
+
+class ChoiceGroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ChoiceGroup
+        fields = ("id", "title", "labor", "get_choice_group_type_display", "number")
+
+
+class ProgramModulesSerializer(serializers.ModelSerializer):
+    choice_group = ChoiceGroupSerializer
+    competence = ProgramCompetenceSerializer
+
+    class Meta:
+        model = ProgramModules
+        fields = ("id", "module__uni_number", "choice_group", "competence", "semester", "index")
 
 
 class ProgramBackup(APIView):
-
     def get(self, request, id):
         program = Program.objects.get(id=id)
         targets = TrainingTarget.objects.filter(program=program)
-        return Response([target.title for target in targets])
+        pms = ProgramModules.objects.filter(program=program)
+        cgs = ChoiceGroup.objects.filter(program=program)
+        comps = ProgramCompetence.objects.filter(program=program)
+
+        serializer = ProgramModulesSerializer(pms)
+
+        return Response(serializer.data)
