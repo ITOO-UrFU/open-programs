385e961fdb45bc45568e134bcca3f7f3bdfd0834
MasterGowen <mastergowen@gmail.com>
2017-08-12 13:18:41 +0500

    epp load#86

--- open_programs/apps/uni/management/commands/epp_load.py
+++ open_programs/apps/uni/management/commands/epp_load.py
@@ -190,7 +190,10 @@ class Command(BaseCommand):
                                                            )
                 variants = Variant.objects.filter(discipline=discipline, program=program, semester__term=cur_term).count()
                 if term == 8:
+                    diagram = Diagram.objects.filter(title="Традиционная очная форма").first()
+                else:
                     diagram = Diagram.objects.filter(title="Традиционная заочная форма").first()
+
                 if variants == 0:
                     Variant.objects.create(discipline=discipline, program=program, semester=semester_obj, diagram=diagram)
 
