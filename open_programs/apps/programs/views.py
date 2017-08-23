eef29b08e0c70a518df740f2e473d88054bd1592
MasterGowen <mastergowen@gmail.com>
2017-08-23 15:47:19 +0500

    prograb backup#5

--- open_programs/apps/programs/views.py
+++ open_programs/apps/programs/views.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Program, TrainingTarget, ProgramModules, ChoiceGroup, ProgramCompetence
-from models import ProgramBackup as PB
+from .models import ProgramBackup as PB
 from disciplines.models import Discipline, Semester, TrainingTerms, Diagram, Technology, Variant
 
 
